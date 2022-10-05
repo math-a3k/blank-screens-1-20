6ec58db0aa6c5b1e5d294e25f9b34d7a90a2c336
Rodrigo <rodrigo@crowdbotics.com>
2022-10-05 20:59:23 +0000

    Add Blank Screen 5

---
+++ screens/BlankScreen5/index.js
@@ -0,0 +1,19 @@
+
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const BlankScreen5 = () => {
+  return <View style={_styles.BlankScreen5}>
+      </View>;
+};
+
+export default BlankScreen5;
+
+const _styles = StyleSheet.create({
+  BlankScreen5: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  }
+});
